76fedd80e23791c6c065df899b06a7444dd3b57d
Ivan Mallory <ivanmallory1992@gmail.com>
2020-12-11 16:00:09 -0600

    Added completed Coderbyte algorithms

--- Coderbyte Challenges/Easy/algorithms.js
+++ Coderbyte Challenges/Easy/algorithms.js
@@ -42,7 +42,43 @@ function LongestWord(sen) {
           longword = senArr[i];	
         }
     }
-   return longword;
+    return longword;
 }
   // keep this function call here 
 console.log(LongestWord(readline()));
+
+
+//Find Intersection
+
+//Have the function FindIntersection(strArr) read the array of strings stored in strArr which will contain 2 elements: the first element will represent a list of comma-separated numbers sorted in ascending order, the second element will represent a second list of comma-separated numbers (also sorted). Your goal is to return a comma-separated string containing the numbers that occur in elements of strArr in sorted order. If there is no intersection, return the string false.
+
+//Examples
+//Input: ["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]
+//Output: 1,4,13
+//Input: ["1, 3, 9, 10, 17, 18", "1, 4, 9, 10"]
+//Output: 1,9,10
+
+
+function FindIntersection(strArr) { 
+  const lists = strArr.map(str => str.split(", "));
+  const firstList = lists[0];
+  const secondList = lists[1];
+
+  let matchMap = {};
+  let resultArr = [];
+
+  firstList.forEach(num => matchMap[num] = true)
+
+  secondList.forEach(num => {
+    if(matchMap[num]){
+      resultArr.push(num);
+    }
+  })
+
+  if(resultArr.length > 0){
+    return resultArr.join(",");
+  }
+  return false;
+}
+// keep this function call here 
+console.log(FindIntersection(readline()));
